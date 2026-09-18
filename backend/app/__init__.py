af44254144b1c6c44d17ae3c8b695f00ce59ee5b
Branice Simaloi <simaloibranice@gmail.com>
2026-09-18 22:15:37 +0300

    chore: move frontend and backend to project root

---
+++ backend/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+from .extensions import db, jwt
+
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    CORS(app)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    @app.get("/api/health")
+    def health_check():
+        return {
+            "status": "ok",
+            "message": "Wasteworx API is running"
+        }
+
+    return app
